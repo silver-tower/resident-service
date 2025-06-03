dd461b690b6a5de6a1fa2b86ed6261c3ff48efd5
sanha <64877575+9sanha@users.noreply.github.com>
2025-06-03 23:06:02 +0900

    init

---
+++ src/main/java/com/silvertown/residentservice/ResidentServiceApplication.java
@@ -0,0 +1,13 @@
+package com.silvertown.residentservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResidentServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResidentServiceApplication.class, args);
+    }
+
+}
