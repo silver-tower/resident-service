06a8176a596ae246caa5366607d8107e32985b62
sanha <64877575+9sanha@users.noreply.github.com>
2025-06-04 02:46:51 +0900

    user domain 생성

--- src/main/java/com/silvertown/resident/ResidentServiceApplication.java
+++ src/main/java/com/silvertown/resident/ResidentServiceApplication.java
@@ -1,4 +1,4 @@
-package com.silvertown.residentservice;
+package com.silvertown.resident;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/silvertown/resident/ResidentServiceApplicationTests.java
+++ src/test/java/com/silvertown/resident/ResidentServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package com.silvertown.residentservice;
+package com.silvertown.resident;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
